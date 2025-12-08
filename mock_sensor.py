e7c53c37baeef368119edc289cd5a1e8d1f4b3c4
GanlingZ <gz2184@nyu.edu>
2025-12-08 03:27:08 -0500

    add mock sensor data sender and incorporate alert system to app.py, also updating html files

---
+++ mock_sensor.py
@@ -0,0 +1,28 @@
+"""Mock Sensor Data Sender."""
+
+from datetime import datetime
+from pymongo import MongoClient
+
+
+MONGO_URI = "mongodb+srv://gz:1234@cluster0.fv25oph.mongodb.net/?appName=Cluster0"
+client = MongoClient(MONGO_URI)
+db = client.smart_apartment_db
+
+
+def trigger_fire():
+    """Simulate a high temperature warning."""
+    print("Simulating fire...")
+    reading = {
+        "timestamp": datetime.utcnow(),
+        "apartment_id": "A-101",
+        "room": "Kitchen",
+        "sensor_type": "temperature",
+        "value": 90.0,
+        "unit": "C",
+    }
+    db.sensor_readings.insert_one(reading)
+    print("Data sent to cloud.")
+
+
+if __name__ == "__main__":
+    trigger_fire()
